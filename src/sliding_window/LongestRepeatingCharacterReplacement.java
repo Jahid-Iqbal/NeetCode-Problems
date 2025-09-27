0e0676fe82e3ae0e6fe71d8779c10d4913362dc9
unknown <jahidiqbal1994@gmail.com>
2025-09-27 22:09:21 +0600

    Medium

---
+++ src/sliding_window/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,39 @@
+package sliding_window;
+
+import java.util.HashSet;
+
+public class LongestRepeatingCharacterReplacement {
+    public int characterReplacement(String s, int k) {
+        int res = 0;
+        HashSet<Character> charSet = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            charSet.add(c);
+        }
+
+        for (char c : charSet) {
+            int count = 0, l = 0;
+            for (int r = 0; r < s.length(); r++) {
+                if (s.charAt(r) == c) {
+                    count++;
+                }
+
+                while ((r - l + 1) - count > k) {
+                    if (s.charAt(l) == c) {
+                        count--;
+                    }
+                    l++;
+                }
+
+                res = Math.max(res, r - l + 1);
+            }
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        char[] ar={'A', 'B', 'A', 'B', 'B', 'A'};
+        String s= "ABABBA";
+        System.out.print(new LongestRepeatingCharacterReplacement().characterReplacement(s,2));
+    }
+}
